447381db19375575bbb3796eaae10797b621ab25
StarSky <254143170@qq.com>
2018-10-17 18:13:54 +0800

    个人信息密码api

--- src/main/java/com/starsky/meteor/db/provider/UserInfoProvider.java
+++ src/main/java/com/starsky/meteor/db/provider/UserInfoProvider.java
@@ -1,10 +1,13 @@
 package com.starsky.meteor.db.provider;
 
 import com.starsky.meteor.db.HibernateFactory;
-import org.hibernate.Hibernate;
+import com.starsky.meteor.db.op.UserInfoEntity;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class UserInfoProvider {
     private final Session session;
@@ -15,12 +18,33 @@ public class UserInfoProvider {
     }
 
     public void CreateUserInfo(String userPhone){
-        String hql="insert into user_info (user_phone) values (?);";
-        NativeQuery sqlQuery = session.createSQLQuery(hql);
-//        sqlQuery.addEntity("userPhone",userPhone);
+        String sql="insert into user_info (user_phone) values (?);";
+        NativeQuery sqlQuery = session.createSQLQuery(sql);
         sqlQuery.setString(0,userPhone);
+        sqlQuery.executeUpdate();
+//        transaction.commit();
+    }
 
+    public void updateUserInfo(String nickName, String password, String phoneNumber){
+        String sql="update  UserInfoEntity user_info set  user_info.userNickname=:userNickname,user_info.userPassword=:userPassword where user_info.userPhone=:userPhone";
+        Query sqlQuery = session.createQuery(sql);
+        sqlQuery.setString("userNickname",nickName);
+        sqlQuery.setString("userPassword",password);
+        sqlQuery.setString("userPhone",phoneNumber);
         sqlQuery.executeUpdate();
         transaction.commit();
+//        session.getTransaction().commit();
+    }
+
+    public UserInfoEntity queryUserInfo(String phoneNumber) throws IndexOutOfBoundsException{
+        Session session = HibernateFactory.getSession();
+        transaction = session.beginTransaction();
+        String sql="select * from user_info where user_phone= :user_phone ";
+        NativeQuery sqlQuery = session.createSQLQuery(sql).addEntity(UserInfoEntity.class);
+        sqlQuery.setParameter("user_phone",phoneNumber);
+        List<UserInfoEntity> list = sqlQuery.list();
+        System.out.println(list.get(0).getUserNickname());
+        transaction.commit();
+        return list.get(0);
     }
 }
--- src/test/java/com/starsky/meteor/db/provider/UserInfoProviderTest.java
+++ src/test/java/com/starsky/meteor/db/provider/UserInfoProviderTest.java
@@ -1,19 +1,33 @@
 package com.starsky.meteor.db.provider;
 
+import com.starsky.meteor.db.op.UserInfoEntity;
 import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserInfoProviderTest {
 
+    UserInfoProvider userInfoProvider = new UserInfoProvider();
+
     @Test
-    void createUserInfo() {
-        UserInfoProvider userInfoProvider = new UserInfoProvider();
+    void testCreateUserInfo() {
         try {
             userInfoProvider.CreateUserInfo("123123");
         }catch (ConstraintViolationException e){
             System.out.println("插入冲突");
         }
+    }
+
+    @Test
+    void testUpdateUserInfo(){
+        userInfoProvider.updateUserInfo("bbb","aaa","123");
+    }
+
+    @Test
+    void testQueryUserInfo(){
+        UserInfoEntity userInfoEntity = userInfoProvider.queryUserInfo("123");
+        System.out.println(userInfoEntity.getId());
+        System.out.println(userInfoEntity.getUserNickname());
+        System.out.println(userInfoEntity.getUserPhone());
+        System.out.println(userInfoEntity.getUserPassword());
     }
 }
