a59b9a08c483a4bba31f584107e061adc3096c93
StarSky <254143170@qq.com>
2018-10-17 12:22:25 +0800

    数据库配置

--- src/main/java/com/starsky/meteor/MeteorApplication.java
+++ src/main/java/com/starsky/meteor/MeteorApplication.java
@@ -1,11 +1,13 @@
 package com.starsky.meteor;
 
+import com.starsky.meteor.db.HibernateFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MeteorApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MeteorApplication.class, args);
     }
